0b7e667dd4d4318367dbe390a494ee33640a6345
oued-multi-service-design <ouedmultiservicedesign@gmail.com>
2025-03-21 02:07:32 +0100

    Update home.js

--- js/home.js
+++ js/home.js
@@ -1,6 +1,3 @@
-
-
-
 document.addEventListener("DOMContentLoaded", () => {
     // Initialize AOS
     AOS.init({
@@ -9,15 +6,33 @@ document.addEventListener("DOMContentLoaded", () => {
     });
 });
 
+const loader = document.querySelector('.loader');
 
-// Initialisation de Swiper.js pour le carrousel de témoignages
-const testimonialSwiper = new Swiper(".swiper", {
-    loop:true,
-    autoplay:{
-        delay:5000
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+window.addEventListener('load', () => {
+
+    loader.classList.add('fondu-out');
+
 })
+
+// Sélectionne le bouton "remonter"
+const boutontop = document.getElementById('remonter-vers-le-haut');
+
+// Fonction pour vérifier la position de défilement
+function toggleboutontop() {
+    if (window.scrollY > 100) { // Si l'utilisateur a défilé de plus de 100 pixels
+        boutontop.style.display = 'block'; // Affiche le bouton
+    } else {
+        boutontop.style.display = 'none'; // Cache le bouton
+    }
+}
+// Écoute l'événement de défilement
+window.addEventListener('scroll', toggleboutontop);
+
+// Fonction pour remonter en haut de la page
+boutontop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Défilement fluide
+    });
+
+});
