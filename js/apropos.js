df7826522f5774a3882b8b857c319b1cb5256a7c
oued-multi-service-design <ouedmultiservicedesign@gmail.com>
2025-03-21 02:06:58 +0100

    Update apropos.js

--- js/apropos.js
+++ js/apropos.js
@@ -5,16 +5,36 @@ document.addEventListener("DOMContentLoaded", () => {
         once: true, // L'animation s'applique une seule fois
     });
 });
+const loader = document.querySelector('.loader');
 
+window.addEventListener('load', () => {
 
-// Initialisation de Swiper.js pour le carrousel de témoignages
-const testimonialSwiper = new Swiper(".swiper", {
-    loop:true,
-    autoplay:{
-        delay:5000
-    },
-    pagination: {
-        el: ".swiper-pagination",
-        clickable: true,
-    }
+    loader.classList.add('fondu-out');
+
 })
+
+// Sélectionne le bouton "remonter"
+const boutontop = document.getElementById('remonter-vers-le-haut');
+
+// Fonction pour vérifier la position de défilement
+function toggleboutontop() {
+    if (window.scrollY > 100) { // Si l'utilisateur a défilé de plus de 100 pixels
+        boutontop.style.display = 'block'; // Affiche le bouton
+    } else {
+        boutontop.style.display = 'none'; // Cache le bouton
+    }
+}
+// Écoute l'événement de défilement
+window.addEventListener('scroll', toggleboutontop);
+
+// Fonction pour remonter en haut de la page
+boutontop.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Défilement fluide
+    });
+
+});
+
+
+
